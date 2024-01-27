e856ee88e1c6c9968e2f98682603cd541bac0229
Errorismx <bsry.nitipoom@gmail.com>
2024-01-27 12:31:58 +0700

    feat: add pl2303driver(door usb) to lib

--- app/src/main/java/com/example/truelabdoor/MainActivity.java
+++ app/src/main/java/com/example/truelabdoor/MainActivity.java
@@ -1,15 +1,14 @@
 package com.example.truelabdoor;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.hardware.usb.UsbManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.text.TextUtils;
 import android.widget.TextView;
 import tw.com.prolific.driver.pl2303.PL2303Driver;
-import tw.com.prolific.driver.pl2303.PL2303Driver.DataBits;
-import tw.com.prolific.driver.pl2303.PL2303Driver.FlowControl;
-import tw.com.prolific.driver.pl2303.PL2303Driver.Parity;
-import tw.com.prolific.driver.pl2303.PL2303Driver.StopBits;
 
 import com.cardlan.twoshowinonescreen.CardLanSerialHelper;
 import com.cardlan.twoshowinonescreen.CardLanStandardBus;
@@ -17,27 +16,114 @@ import com.cardlan.utils.ByteUtil;
 import com.example.truelabdoor.data.TerminalConsumeDataForSystem;
 import com.example.truelabdoor.util.QRJoint;
 
-import java.io.FileDescriptor;
+//import java.io.FileDescriptor;
+import java.io.IOException;
 
 
 public class MainActivity extends AppCompatActivity {
     PL2303Driver mSerial;
-    CardLanStandardBus mCardLanDevCtrl = new CardLanStandardBus();
-    private FileDescriptor QrCodeFlag = mCardLanDevCtrl.callSerialOpen("/dev/ttyAMA4", 115200, 0);
+    private final PL2303Driver.BaudRate mBaudrate = PL2303Driver.BaudRate.B9600;
+    private final PL2303Driver.DataBits mDataBits = PL2303Driver.DataBits.D8;
+    private final PL2303Driver.Parity mParity = PL2303Driver.Parity.NONE;
+    private final PL2303Driver.StopBits mStopBits = PL2303Driver.StopBits.S1;
+    private final PL2303Driver.FlowControl mFlowControl = PL2303Driver.FlowControl.OFF;
+    String TAG = "PL2303HXD_APLog";
+
+    private static final String ACTION_USB_PERMISSION = "com.example.truelabdoor.USB_PERMISSION";
+
+//    CardLanStandardBus mCardLanDevCtrl = new CardLanStandardBus();
+
+//    private final FileDescriptor QrCodeFlag = mCardLanDevCtrl.callSerialOpen("/dev/ttyAMA4", 115200, 0);
     private TextView mTv_qc_result;
     TerminalConsumeDataForSystem terminal;
+    private void openUsbSerial() {
+        Log.d(TAG, "Enter  openUsbSerial");
 
+        if(mSerial==null) {
+
+            Log.d(TAG, "No mSerial");
+            return;
+
+        }
+
+
+        if (mSerial.isConnected()) {
+//            if (SHOW_DEBUG) {
+//                Log.d(TAG, "openUsbSerial : isConnected ");
+//            }
+//            mBaudrate = PL2303Driver.BaudRate.B9600;
+//            String str = PL2303HXD_BaudRate_spinner.getSelectedItem().toString();
+//            int baudRate= Integer.parseInt(str);
+//            switch (baudRate) {
+//                case 9600:
+//                    mBaudrate = PL2303Driver.BaudRate.B9600;
+//                    break;
+//                case 19200:
+//                    mBaudrate =PL2303Driver.BaudRate.B19200;
+//                    break;
+//                case 115200:
+//                    mBaudrate =PL2303Driver.BaudRate.B115200;
+//                    break;
+//                default:
+//                    mBaudrate =PL2303Driver.BaudRate.B9600;
+//                    break;
+//            }
+//            Log.d(TAG, "baudRate:"+baudRate);baudRate
+            // if (!mSerial.InitByBaudRate(mBaudrate)) {
+            if (!mSerial.InitByBaudRate(mBaudrate,700)) {
+/*
+                if(!mSerial.PL2303Device_IsHasPermission()) {
+                   Toast.makeText(this, "cannot open, maybe no permission", Toast.LENGTH_SHORT).show();
+                }
+*/
+
+                if(mSerial.PL2303Device_IsHasPermission() && (!mSerial.PL2303Device_IsSupportChip())) {
+//                    Toast.makeText(this, "cannot open, maybe this chip has no support, please use PL2303HXD / RA / EA chip.", Toast.LENGTH_SHORT).show();
+                    Log.d(TAG, "cannot open, maybe this chip has no support, please use PL2303HXD / RA / EA chip.");
+                }
+            } else {
+
+//                Toast.makeText(this, "connected : OK" , Toast.LENGTH_SHORT).show();
+                Log.d(TAG, "connected : OK");
+                Log.d(TAG, "Exit  openUsbSerial");
+
+
+            }
+        }//isConnected
+        else {
+//            Toast.makeText(this, "Connected failed, Please plug in PL2303 cable again!" , Toast.LENGTH_SHORT).show();
+            Log.d(TAG, "connected failed, Please plug in PL2303 cable again!");
+
+
+        }
+    }
     protected boolean validateQrCode(String qrcode){
         return true;
     }
 
     protected void openDoor(){
+        openUsbSerial();
+        if(null==mSerial)
+            return;
 
+        if(!mSerial.isConnected())
+            return;
+        System.out.println("open");
+        String strWrite = "a";
+        mSerial.write(strWrite.getBytes(), strWrite.length());
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mSerial = new PL2303Driver((UsbManager) getSystemService(Context.USB_SERVICE),
+                this, ACTION_USB_PERMISSION);
+        try {
+            mSerial.setup(mBaudrate, mDataBits, mStopBits, mParity, mFlowControl);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
         setContentView(R.layout.activity_main);
         mTv_qc_result = findViewById(R.id.tv_serial_result);
         terminal = new TerminalConsumeDataForSystem();
@@ -47,7 +133,7 @@ public class MainActivity extends AppCompatActivity {
         System.out.println("Scan your QR code");
         serialTest.setCallBack((buffer, size) -> {
             System.out.println("Scanning");
-            if (buffer == null || buffer.length <= 0) {
+            if (buffer == null || buffer.length == 0) {
                 return;
             }
 
@@ -57,24 +143,19 @@ public class MainActivity extends AppCompatActivity {
             System.out.println(realQRCode);
 
             Log.d("qrCode", "QrCode=" + qrCode);
-            if (TextUtils.isEmpty(qrCode)) {
-                return;
-            }
+            if (TextUtils.isEmpty(qrCode)) return;
 
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    if (ByteUtil.notNull(qrCode)) {
-
-                        mTv_qc_result.setText(": " + qrCode);
-                        if (validateQrCode(qrCode)) {
-                            openDoor();
-                        }
-                        terminal.callProc();
-                        System.out.println("Done");
+            runOnUiThread(() -> {
+                if (ByteUtil.notNull(qrCode)) {
+                    String qrFormat = ": " + qrCode;
+                    mTv_qc_result.setText(qrFormat);
+                    if (validateQrCode(qrCode)) {
+                        openDoor();
                     }
-
+                    terminal.callProc();
+                    System.out.println("Done");
                 }
+
             });
         });
     }
