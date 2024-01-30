b56bb77182057d4c181835d099d9221f688c372b
Errorism <bsry.nitipoom@gmail.com>
2024-01-30 17:01:36 +0700

    feat: SerialIO, onStart refactor: serial

---
+++ app/src/main/java/com/example/truelabdoor/Constants.java
@@ -0,0 +1,6 @@
+package com.example.truelabdoor;
+
+public class Constants {
+    public static final String LOG_TAG = "com.example.truelabdoor";
+    public static final String TOKEN = "";
+}
---
+++ app/src/main/java/com/example/truelabdoor/StartBoot.java
@@ -0,0 +1,17 @@
+package com.example.truelabdoor;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import java.util.Objects;
+
+public class StartBoot extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (Objects.equals(intent.getAction(), Intent.ACTION_BOOT_COMPLETED)) {
+            Intent i = new Intent(context, MainActivity.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(i);
+        }
+    }
+}
